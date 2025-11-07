4104c6536c931c3223523d23b628754498b1ff9d
Roland PJ <rolandpj@gmail.com>
2025-11-07 21:37:11 +0200

    More 3.4 BasicObject intrinsics

--- lib/core/3.4/basic_object.rb
+++ lib/core/3.4/basic_object.rb
@@ -1,3 +1,13 @@
 class BasicObject
-  def __id__ = __intrinsic__('::Rurubyby::Vm::ObjectObject', :rurubyby__id__, self)
+  # TODO - I suspect we'll need to establish separate Rurubyby classes for any ruby class that requires intrinsics
+  #   Specifically between BasicObject, Kernel and Object
+
+  def __id__ = __intrinsic__('::Rurubyby::Vm::ObjectObject', :rurubyby___id__, self)
+
+  def ! = __intrinsic__('::Rurubyby::Vm::ObjectObject', :rurubyby__not_, self)
+
+  def equal?(v) = __intrinsic__('::Rurubyby::Vm::ObjectObject', :rurubyby_equal?, self, v)
+
+  # TODO - not sure if this and equal? are entirely separate in ruby semantics
+  def ==(v) = __intrinsic__('::Rurubyby::Vm::ObjectObject', :rurubyby__equals_equals_, self, v)
 end
--- lib/rurubyby/vm/object_object.rb
+++ lib/rurubyby/vm/object_object.rb
@@ -34,7 +34,13 @@ module Rurubyby
         # Intrinsics
         #
 
-        def rurubyby__id__(v) = v.__id__
+        def rurubyby___id__(v) = v.__id__
+
+        def rurubyby__not_(v) = (v.equal?(FalseObject::FALSE_OBJECT) || v.equal?(NilObject::NIL_OBJECT)) ? TrueObject::TRUE_OBJECT : FalseObject::FALSE_OBJECT
+
+        def rurubyby_equal?(v1, v2) = v1.__id__.equal?(v2.__id__)
+
+        def rurubyby__equals_equals_(v1, v2) = v1.__id__.equal?(v2.__id__)
       end
     end
   end
