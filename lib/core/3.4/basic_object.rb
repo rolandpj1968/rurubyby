d20d175d2513754a9f99a332a6e3ea1143691f53
Roland PJ <rolandpj@gmail.com>
2025-11-11 16:03:54 +0200

    reshuffling intrinsics... going to do it one more time

--- lib/core/3.4/basic_object.rb
+++ lib/core/3.4/basic_object.rb
@@ -1,13 +1,9 @@
 class BasicObject
-  # TODO - I suspect we'll need to establish separate Rurubyby classes for any ruby class that requires intrinsics
-  #   Specifically between BasicObject, Kernel and Object
+  def __id__    = __intrinsic__('::Rurubyby::Intrinsics::BasicObject', :____id__, self)
 
-  def __id__ = __intrinsic__('::Rurubyby::Vm::ObjectObject', :rurubyby___id__, self)
+  def !         = __intrinsic__('::Rurubyby::Intrinsics::BasicObject', :___not_, self)
 
-  def ! = __intrinsic__('::Rurubyby::Vm::ObjectObject', :rurubyby__not_, self)
+  def equal?(v) = __intrinsic__('::Rurubyby::Intrinsics::BasicObject', :__equal?, self, v)
 
-  def equal?(v) = __intrinsic__('::Rurubyby::Vm::ObjectObject', :rurubyby_equal?, self, v)
-
-  # TODO - not sure if this and equal? are entirely separate in ruby semantics
-  def ==(v) = __intrinsic__('::Rurubyby::Vm::ObjectObject', :rurubyby__equals_equals_, self, v)
+  def ==(v)     = __intrinsic__('::Rurubyby::Intrinsics::BasicObject', :___equals_equals_, self, v)
 end
